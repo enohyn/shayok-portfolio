89f1ebc782d3f52e5212887aa70aaf13c40278a7
enohyn <shayokullah@gmail.com>
2023-07-21 12:07:32 +0600

    timeline tweaked

--- src/components/Home/ProjectExperience.jsx
+++ src/components/Home/ProjectExperience.jsx
@@ -9,8 +9,8 @@ const ProjectExperience = () => {
                         <div className=' overflow-y-scroll'>
                         <ol class="timeline text-white ml-4 ">
                             <li class="mb-10 ml-4 relative ">
-                                <span class="absolute  inline-flex justify-center items-center w-8 h-8 bg-gray-200 rounded-full mt-2 -translate-x-1/2 -left-5 border border-white dark:border-gray-900 dark:bg-gray-700 ">
-                                    <FiCode color='#134e4a' className='z-10 text-xl 
+                                <span class="absolute  inline-flex justify-center items-center w-8 h-8 bg-gradient-to-br from-zinc-900 via-zinc-700 to-teal-900 rounded-full mt-2 -translate-x-1/2 -left-5 border border-teal-700 dark:border-gray-900 dark:bg-gray-700 ">
+                                    <FiCode color='#d1d5db' className='z-10 text-xl 
                             rotate-6' width={8} height={8} />
                                 </span>
                                 <div className='p-6 ml-3 backdrops hover:scale-[100%] scale-[98%] transition-all right-arrow top-3 text-gray-400 mt-0 rounded-s-none rounded-lg'>
@@ -21,8 +21,8 @@ const ProjectExperience = () => {
 
                             </li>
                             <li class="mb-10 ml-4  relative ">
-                                <span class="absolute inline-flex justify-center items-center w-8 h-8 bg-gray-200 rounded-full mt-2 -translate-x-1/2 -left-5 border border-white dark:border-gray-900 dark:bg-gray-700 ">
-                                    <FiCode color='#134e4a' className='z-10 text-xl 
+                                <span class="absolute  inline-flex justify-center items-center w-8 h-8 bg-gradient-to-br from-zinc-900 via-zinc-700 to-teal-900 rounded-full mt-2 -translate-x-1/2 -left-5 border border-teal-700 dark:border-gray-900 dark:bg-gray-700 ">
+                                    <FiCode color='#d1d5db' className='z-10 text-xl 
                             rotate-6' width={8} height={8} />
                                 </span>
                                 <div className='p-6 ml-3 backdrops hover:scale-[100%] scale-[98%] transition-all right-arrow top-3 text-gray-400 mt-0 rounded-s-none rounded-lg'>
@@ -33,8 +33,8 @@ const ProjectExperience = () => {
 
                             </li>
                             <li class="mb-10 ml-4  relative ">
-                                <span class="absolute inline-flex justify-center items-center w-8 h-8 bg-gray-200 rounded-full mt-2 -translate-x-1/2 -left-5 border border-white dark:border-gray-900 dark:bg-gray-700 ">
-                                    <FiCode color='#134e4a' className='z-10 text-xl 
+                                <span class="absolute  inline-flex justify-center items-center w-8 h-8 bg-gradient-to-br from-zinc-900 via-zinc-700 to-teal-900 rounded-full mt-2 -translate-x-1/2 -left-5 border border-teal-700 dark:border-gray-900 dark:bg-gray-700 ">
+                                    <FiCode color='#d1d5db' className='z-10 text-xl 
                             rotate-6' width={8} height={8} />
                                 </span>
                                 <div className='p-6 ml-3 backdrops hover:scale-[100%] scale-[98%] transition-all right-arrow top-3 text-gray-400 mt-0 rounded-s-none rounded-lg'>
@@ -45,8 +45,8 @@ const ProjectExperience = () => {
 
                             </li>
                             <li class="ml-4 mb-10 relative ">
-                                <span class="absolute inline-flex justify-center items-center w-8 h-8 bg-gray-200 rounded-full mt-2 -translate-x-1/2 -left-5 border border-white dark:border-gray-900 dark:bg-gray-700 ">
-                                    <FiCode color='#134e4a' className='z-10 text-xl 
+                                <span class="absolute  inline-flex justify-center items-center w-8 h-8 bg-gradient-to-br from-zinc-900 via-zinc-700 to-teal-900 rounded-full mt-2 -translate-x-1/2 -left-5 border border-teal-700 dark:border-gray-900 dark:bg-gray-700 ">
+                                    <FiCode color='#d1d5db' className='z-10 text-xl 
                             rotate-6' width={8} height={8} />
                                 </span>
                                 <div className='p-6 ml-3 backdrops hover:scale-[100%] scale-[98%] transition-all right-arrow top-3 text-gray-400 mt-0 rounded-s-none rounded-lg'>
@@ -57,8 +57,8 @@ const ProjectExperience = () => {
 
                             </li>
                             <li class="ml-4  relative ">
-                                <span class="absolute inline-flex justify-center items-center w-8 h-8 bg-gray-200 rounded-full mt-2 -translate-x-1/2 -left-5 border border-white dark:border-gray-900 dark:bg-gray-700 ">
-                                    <FiCode color='#134e4a' className='z-10 text-xl 
+                                <span class="absolute  inline-flex justify-center items-center w-8 h-8 bg-gradient-to-br from-zinc-900 via-zinc-700 to-teal-900 rounded-full mt-2 -translate-x-1/2 -left-5 border border-teal-700 dark:border-gray-900 dark:bg-gray-700 ">
+                                    <FiCode color='#d1d5db' className='z-10 text-xl 
                             rotate-6' width={8} height={8} />
                                 </span>
                                 <div className='p-6 ml-3 backdrops hover:scale-[100%] scale-[98%] transition-all right-arrow top-3 text-gray-400 mt-0 rounded-s-none rounded-lg'>
