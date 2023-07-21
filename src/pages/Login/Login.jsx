0f0e506ae7a8d2d26a15f21b8e86e86b7ad4af5b
enohyn <shayokullah@gmail.com>
2023-07-21 22:38:30 +0600

    Login Page Added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header'
+import AuthUser from '../../components/UserLogin/AuthUser'
+
+const Login = () => {
+  return (
+    <div>
+        <Header/>
+        <AuthUser/>
+    </div>
+  )
+}
+
+export default Login
