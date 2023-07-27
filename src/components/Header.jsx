612d859f85ff99c48a8accf5d2f5f90f3630d4a4
enohyn <shayokullah@gmail.com>
2023-07-27 16:44:05 +0600

    Mobile menu added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineLinkedin } from "react-icons/ai";
 import { VscGithub } from 'react-icons/vsc';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 
 import { NavLink } from 'react-router-dom';
 import { MENU_PATHS } from '../constants/MENU_PATHS';
+
 function Header() {
   const ListClass = `text-white `
-
+  const [clickState, setClickState] = useState(false);
+  const menuClick = () => {
+    setClickState(!clickState);
+  }
 
   return (
     <header className='bg-gradient-to-br from-zinc-900 via-zinc-900 to-teal-900 h-[8vh] sticky top-0 z-20 flex items-center '>
       <nav className='py-4 px-7 w-full container mx-auto'>
-        <div className='flex justify-around items-center'>
+        <div className='max-lg:hidden flex justify-around items-center'>
           <ul className='list-none capitalize flex items-center justify-start w-1/3'>
             {MENU_PATHS.map((item, index) => (
               <li key={index} className={ListClass}>
@@ -27,13 +33,44 @@ function Header() {
           </a>
           <div className='flex gap-x-4 w-1/3 justify-end '>
             <NavLink to={`https://github.com/enohyn`} target='_blank'>
-              <VscGithub className='w-8 h-8 text-white hover:text-teal-600 transition'/>
+              <VscGithub className='w-8 h-8 text-white hover:text-teal-600 transition' />
             </NavLink>
             <NavLink to={`https://www.linkedin.com/in/shayok-here/`} target='_blank'>
               <AiOutlineLinkedin className='w-8 h-8 text-white hover:text-teal-600 transition' />
             </NavLink>
           </div>
-         
+        </div>
+          
+          {/* Mobile Menu */}
+        <div className='lg:hidden flex justify-around items-center'>
+          <span className='w-1/3 flex justify-start'>
+            <GiHamburgerMenu className='text-white' onClick={menuClick} />
+          </span>
+          <ul className={clickState ? `list-none capitalize w-2/3 h-[calc(100vh_-_5vh)] absolute top-20 left-0 py-10 flex flex-col justify-start transition-all backdrops` : 'hidden left-20'}>
+            {MENU_PATHS.map((item, index) => (
+              <li key={index} className={`${ListClass} py-5 w-full flex flex-col items-center`}>
+                <a href={item.path} className={`font-space focus:backdrops rounded-sm text-white w-full text-center py-3`}
+                  >
+                  {item.title}
+                </a>
+              </li>
+            ))}
+            <li>
+              <div className="flex justify-center items-end gap-x-3">
+                <NavLink to={`https://github.com/enohyn`} target='_blank'>
+                  <VscGithub className='w-8 h-8 text-white hover:text-teal-600 transition' />
+                </NavLink>
+                <NavLink to={`https://www.linkedin.com/in/shayok-here/`} target='_blank'>
+                  <AiOutlineLinkedin className='w-8 h-8 text-white hover:text-teal-600 transition' />
+                </NavLink>
+              </div>
+            </li>
+          </ul>
+          <a className='inline-flex w-1/3 justify-center items-center' href='/shayok-portfolio'>
+            <h4 className='lg:brand-logo-text font-megrim text-xl text-white hover:text-teal-600 transition'>Shayok</h4>
+          </a>
+          <div className='flex gap-x-4 w-1/3 justify-end '>
+          </div>
         </div>
       </nav>
     </header>
